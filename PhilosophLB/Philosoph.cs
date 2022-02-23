243bf1569f24ade7dec0a30af3f87b2ee16dca58
student <student@SSD2205>
2022-02-23 12:21:41 +0200

    Добавьте файлы проекта.

---
+++ PhilosophLB/Philosoph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilosophLB
+{
+    class Philosoph
+    {
+        public object leftFork { get; set; }
+        public object RightFork { get; set; }
+        public bool isHungry { get; set; } = true;
+        public void Eat()
+        {
+            Console.WriteLine("Философ ест");
+
+        }
+        public void Full()
+        {
+            Console.WriteLine("Философ наелся ");
+            isHungry = false;
+        }
+        public void Doom()
+        {
+            Console.WriteLine("Философ думает");
+        }
+    }
+}
---
+++ PhilosophLB/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+
+namespace PhilosophLB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart TS = new ThreadStart(Game);
+
+            Thread T = new Thread(TS);
+            T.Start();
+            Console.ReadLine();
+        }
+
+        public static void Game()
+        {
+            Philosoph[] p = new Philosoph[5];
+
+
+
+            object[] fork = new object[5];
+            for (int i = 0; i < 5; i++)
+            {
+                fork[i] = new object();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                    p[i] = new Philosoph() { leftFork = fork[i], RightFork = fork[0] };
+                else
+                    p[i] = new Philosoph() { leftFork = fork[i], RightFork = fork[i + 1] };
+            }
+
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                while (p[i].isHungry == true)
+                {
+                    if (Monitor.TryEnter(p[i].leftFork))
+                    {
+                        if (Monitor.TryEnter(p[i].RightFork))
+                        {
+                            try
+                            {
+                                p[i].Eat();
+                            }
+                            finally
+                            {
+                                if (Monitor.IsEntered(p[i].RightFork))
+                                {
+                                    Monitor.Exit(p[i].RightFork);
+                                    Console.WriteLine("Философ положил правую вилку");
+                                }
+                            }
+                        }
+                        try
+                        {
+                            p[i].Full();
+                        }
+                        finally
+                        {
+                            if (Monitor.IsEntered(p[i].leftFork))
+                            {
+                                Monitor.Exit(p[i].leftFork);
+                                Console.WriteLine("Философ положил левую вилку");
+
+
+
+                            }
+                        }
+
+
+
+                    }
+                }
+                if (p[i].isHungry == false)
+                {
+                    p[i].Doom();
+                }
+            }
+        }
+    }
+}
